195789e798035b436bb0400315ce0c9020911d3b
Michael Angerman <stormasm@gmail.com>
2016-12-27 18:07:58 -0800

    rename packages

--- index/example.go
+++ index/example.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a MIT-license.
 // See http://olivere.mit-license.org/license.txt for details.
 
-package mytest
+package index
 
 import (
 	"encoding/json"
---
+++ tindex0.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/stormasm/elastic-examples/index"
+)
+
+func main() {
+	index.ExampleBodyString()
+}
